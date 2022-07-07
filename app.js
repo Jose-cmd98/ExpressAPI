e9b06c7c7d9fa24ae6f12c53a8f195900e937a7e
Jose-cmd98 <jose1234roberto@gmail.com>
2022-07-07 17:52:38 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+// imports neededs
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
+const app = express(); // create express app
+const port = 3000
+
+//config express to understand json
+app.use(express.json());
+
+//example route
+app.get('/', (req, res) => {
+    res.status(200).json({msg: "Welcome to the API"});
+})
+
+
+//user routes
+const userRoutes = require('./routes/userRoutes');
+
+//register user
+app.use('/auth/register', userRoutes);
+
+//login user
+const login = require('./routes/loginRoutes');
+app.use('/auth/login', login);
+
+
+
+
+
+
+
+// Credentials of the database
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+
+
+// Connect to MongoDB
+mongoose.connect(
+    `mongodb+srv://${dbUser}:${dbPassword}@authjwt.n3icehj.mongodb.net/?retryWrites=true&w=majority`
+)
+.then(()=>{
+    console.log('Connected to MongoDB');
+    app.listen(port , console.log('Server has started on port ' + port));
+
+})
+.catch(err => console.log(err));
---
+++ routes/loginRoutes.js
@@ -0,0 +1,47 @@
+const router = require('express').Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/User');
+
+
+router.post('/', async (req, res)=>{
+
+    const { email, password } = req.body;
+
+    if(!email){
+        res.status(404).json({msg: "Email Obrigatório"});
+    }
+    if(!password){
+        res.status(404).json({msg: "Senha Obrigatória"});
+    }
+
+    //check if exixts
+    const user = await User.findOne({email: email});
+    if(!user){
+        return res.status(404).json({msg: "Usuário não existe"});
+    }
+
+    const checkPassword = await bcrypt.compare(password, user.password);
+    if(!checkPassword){
+        return res.status(404).json({msg: "Senha Incorreta"});
+    }
+    try{
+
+        const secret = process.env.SECRET;
+
+        const token = jwt.sign({
+            id: user._id,
+        },
+            secret,
+        )
+        res.status(200).json({msg: "Usuário autenticado", token: token, user: user.name});
+
+    } catch (error){
+        console.log(error);
+        res.status(500).json({msg: "Erro no servidor"});
+    }
+})
+
+
+module.exports = router;
